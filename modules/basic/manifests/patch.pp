b87095c48b548db6c642da912b5b01f802eb2b22
unknown <U0124075@U0124075-TPL-A.ten.thomsonreuters.com>
2015-05-19 20:47:25 +0800

    return code

--- modules/basic/manifests/patch.pp
+++ modules/basic/manifests/patch.pp
@@ -4,12 +4,13 @@ class basic::patch {
     
     exec{ "python3_install":
         command => "yum -y install scl-utils && rpm -Uvh https://www.softwarecollections.org/en/scls/rhscl/python33/epel-7-x86_64/download/rhscl-python33-epel-7-x86_64.noarch.rpm && yum -y install python33",
-        returns => [0, 128],
+        returns => [0, 1, 128],
         path    => ['/usr/bin'],
     }
     
     exec{ "you-get_install":
         command => "cd /root/workspace && rm -rf /root/workspace/you-get &&git clone https://github.com/soimort/you-get.git",
+        returns => [0, 1, 128],
         path => ['/usr/bin'],
     }
     
