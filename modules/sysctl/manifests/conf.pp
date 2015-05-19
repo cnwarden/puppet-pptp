ef32fbcddef77c72815ff084815aad5edeb8c228
ming.he <ming.he@thomsonreuters.com>
2015-05-19 11:23:40 +0800

    package installed

--- modules/sysctl/manifests/conf.pp
+++ modules/sysctl/manifests/conf.pp
@@ -17,18 +17,3 @@ define sysctl::conf ( $value ) {
    }
 
 }
-
-# /etc/puppet/modules/sysctl/manifests/init.pp
-class sysctl {
-   file { "sysctl_conf":
-      name => $operatingsystem ? {
-        default => "/etc/sysctl.conf",
-      },
-   }
-
-   exec { "sysctl -p":
-      alias       => "sysctl",
-      refreshonly => true,
-      subscribe   => File["sysctl_conf"],
-   }
-}
